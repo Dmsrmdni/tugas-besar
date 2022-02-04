d5cfeea512abd83679f81aebd61bcbca0b7bc7a6
Dimas Ramdani <94954694+Dmsrmdni@users.noreply.github.com>
2022-02-05 01:07:42 +0700

    Add files via upload

---
+++ contacus.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <title>Contact Us</title>
+  </head>
+  <body style="background-color: rgb(59, 59, 59);">
+    <!-- NAVBAR -->
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="">
+            <img src="Logo.png" width="60px" height="60px" style="border-radius: 100%;">&nbsp; Grand Blue Hotel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-left: 700px;">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="beranda1.php">Beranda <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="fasilitas.php">Fasilitas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="kamar2.php">Kamar</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contacus.php">Contact Us</a>
+          </ul>
+        </div>
+    </nav>
+    <br>
+
+     <!-- ISI -->
+    <center>
+
+      <font face = "Copperplate"><h2 style = "color: white;">Hubungi Kami</h2></font>
+
+      <br>
+
+    <div class="card" style="width: 20rem;">
+      <center>
+        <img src="Logo.png" width="210" height="170">
+    </center>
+      <div class="card-body">
+        <form action="https://smkassalaambandung.sch.id/">
+          <table>
+            <tr>
+              <td>Nama </td>
+              <td> : </td>
+              <td> <input type="text" name="nama"></td>
+            </tr>
+            <tr>
+              <td>Email </td>
+              <td> : </td>
+              <td> <input type="email" name="email"></td>
+            </tr>
+            <tr>
+              <td>pesan </td>
+              <td> : </td>
+              <td> <textarea name="pesan" id="" cols="25" rows="2"></textarea> </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td> <button type="submit">kirim</button> </td>
+            </tr>
+          </table>
+        </form>
+      </div>
+    </div>
+    
+  
+  </center>
+
+      <br><br>
+    <!-- FOOTER -->
+    <hr>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="foot-logo">
+                            <img src="https://smkassalaambandung.sch.id/img/logo-custom.png" class="img-fluid" alt="footer_logo" width="90" arrange.php.li>
+                        <p>2021 © copyright by <a target="_blank">Smk Assalaam Bandung</a> <br>Tim 1</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="sitemap">
+                            <h3 style="color: white;">Menu Utama</h3>
+                            <ul>
+                              <li><a href="beranda1.php">Beranda</a></li>
+                              <li><a href="fasilitas.php">Fasilitas </a></li>
+                              <li><a href="kamar2.php">Kamar</a></li>
+                              <li><a href="contacus.php">Contact Us</a></li>
+                            </ul>
+                        </div>
+    
+                    </div>
+                    <div class="col-md-3">
+                        <div class="address">
+                            <h3 style="color: white;">Hubungi Kami</h3>
+                            <p><span>Alamat : </span> Smk Assalaam Bandung
+                                <br><span>Email :</span> Smkassalaambandung.sch.id
+                                <br><span> Phone :</span> 081236702991</p>
+                                    <img src="wa.png" alt="" width="85" height="65">
+                                    <img src="ig.png" alt="" width="80" height="60">
+                                    <img src="fb.png" alt="" width="65" height="45">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
